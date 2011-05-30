9190db58a27d149a6f789b60bce1ff1e41819704
mmanager <mmanager9000@gmail.com>
2011-05-30 21:35:35 +0300

    ticket #13 commit

--- testproject/testapp/admin.py
+++ testproject/testapp/admin.py
@@ -4,6 +4,10 @@ from django.contrib import admin
 
 from models import InfoRecord, RequestStore, DatabaseLog
 
+class RequestStoreOptions(admin.ModelAdmin):
+	list_filter = ('req_priority', )
+	list_display = ('req_priority', 'req_date', 'req_method', 'req_path')
+
 admin.site.register(InfoRecord)
-admin.site.register(RequestStore)
+admin.site.register(RequestStore, RequestStoreOptions)
 admin.site.register(DatabaseLog)
